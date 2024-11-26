ae3dc7957343a55cb34e79d130a7302f8605c53c
tatreaso <sulteszrolcsi2@gmail.com>
2024-11-26 18:19:05 +0100

    tizenhet es nyolc

---
+++ 17.py
@@ -0,0 +1,6 @@
+# ki írás
+
+n = int(input("Mennyi legyen: "))
+sztring = "XO"
+for i in range(1, n):
+    print(f"{sztring*n}")
---
+++ 18.py
@@ -0,0 +1,10 @@
+# ki írás 2
+
+n = int(input("Mennyi legyen: "))
+for i in range(1, n):
+    sztring = "XO"
+    print(f"{(sztring*n)}")
+    sztring = "OX"
+    print(f"{(sztring*n)}")
+    sztring = "XO"
+    print(f"{(sztring*n)}")
